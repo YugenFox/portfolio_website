f4c5cba8dee4c5612747c9e3b8c52f66d821f19f
YugenFox <kalebsday@gmail.com>
2023-06-16 16:48:46 -0500

    Hero done, now Projects
    9:06:06

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+      {/* Welcome To <br />
+        My Personal Portfolio */}
+        Hi, I'm Kaleb your next software engineer. (add 3 flags)
+      </SectionTitle>
+      <SectionText>Multicultural Software Engineer who loves working with unique people to make exceptional products.</SectionText>
+      <Button onClick={() => window.location = 'https://google.com/should prob make this jump to my projects part of page'}>Learn More - (fix link)</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
