61b3027c4ef3f92dda05d5ab0225bbb187de12c4
YugenFox <kalebsday@gmail.com>
2023-06-16 22:51:52 -0500

    Accomplishments done, now Footer
    9:36:30

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -2,18 +2,30 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { AccomplishmentsData } from "../../constants/constants";
 
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
+//Use accomplishmentsData to create a list of cards from constants
+// const data = [
+//   { number: 20, text: 'Open Source Projects'},
+//   { number: 1000, text: 'Students', },
+//   { number: 1900, text: 'Github Followers', },
+//   { number: 5000, text: 'Github Stars', }
+// ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>
+      Personal Accomplishments
+    </SectionTitle>
+    <Boxes>
+      {AccomplishmentsData.map((card,index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
