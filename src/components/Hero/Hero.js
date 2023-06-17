b5fc5f7d5258ba45b20f1198a0dbeaf962f206c1
YugenFox <kalebsday@gmail.com>
2023-06-16 20:47:10 -0500

    Timeline complete, next Accomplishments
    9:33:58

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,8 @@ const Hero = () => (
       <SectionTitle main center>
       {/* Welcome To <br />
         My Personal Portfolio */}
-        Hi, I'm Kaleb your next software engineer. (add 3 flags)
+        Hi, I'm Kaleb Day
+        {/* Hi, I'm Kaleb your next software engineer. (add 3 flags) Some words cut off on tablet size 700-800px ish */}
       </SectionTitle>
       <SectionText>Multicultural Software Engineer who loves working with unique people to make exceptional products.</SectionText>
       <Button onClick={() => window.location = 'https://google.com/should prob make this jump to my projects part of page'}>Learn More - (fix link)</Button>
